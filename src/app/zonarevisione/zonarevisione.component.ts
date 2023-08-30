b2a981ff0efa0a83e94795eeb065d53e9471f700
Fabio <f.massa5@libero.it>
2023-08-30 19:08:35 +0200

    terminato zona revisione, con chiamata api per accetta e rifiuta

--- src/app/zonarevisione/zonarevisione.component.ts
+++ src/app/zonarevisione/zonarevisione.component.ts
@@ -35,30 +35,46 @@ export class ZonarevisioneComponent implements OnInit {
     this.updateRequestStatus(requestId, status, userId);
   }
 
-  rejectRequest(idRequest:number, isAccepted:boolean, idUser:number): void {
-    const requestId = idRequest;
-    const status = isAccepted;
-    const userId = idUser;
+  rejectRequest(idrequest:number, isaccepted:boolean, userid:number): void {
+    const idRequest = idrequest;
+    const isAccepted = isaccepted;
+    const userId = userid;
+
+    console.log(idRequest);
+
 
     // Chiamata HTTP per accettare la richiesta
-    this.updateRequestStatus(requestId, status, userId);
+    this.updateRequestStatus(idRequest, isAccepted, userId);
   }
 
   private updateRequestStatus(
-    requestId: number,
-    status: boolean,
+    idRequest: number,
+    isAccepted: boolean,
     userId: number
   ): void {
-    this.http.get<responseRequestAdmin>('http://localhost:5067/AdminRequest?id=' + requestId, { observe: "response"}).subscribe(
-      (response) => {
-        this.requestAdmins = response.body?.data;
-      },
-      (error) => {
-        console.error('Errore nel recupero dei dati:', error);
-      });
-  }
+    const PutReqDto = {
+      idRequest: idRequest,
+      isAccepted: isAccepted,
+      userId: userId
+    };
+
+    console.log(PutReqDto);
 
 
+    this.http.put<any>('http://localhost:5067/AdminRequest?id='+idRequest, PutReqDto).subscribe(
+    (response) => {
+      console.log('welaa');
+
+    },
+    (error) => {
+      console.error('Errore durante la richiesta:', error);
+    }
+  );
+
+
+
+
+}
 }
 
 
