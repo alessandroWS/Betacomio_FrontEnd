ae452bd26386f36adc6a98a562a7ad8cede65653
Fabio <f.massa5@libero.it>
2023-08-30 16:26:27 +0200

    zona revisione, chiamata accetta e rifiuta

--- src/app/zonarevisione/zonarevisione.component.ts
+++ src/app/zonarevisione/zonarevisione.component.ts
@@ -26,10 +26,45 @@ export class ZonarevisioneComponent implements OnInit {
       });
   }
 
+  acceptRequest(idRequest:number, isAccepted:boolean, idUser:number): void {
+    const requestId = idRequest;
+    const status = isAccepted;
+    const userId = idUser;
+
+    // Chiamata HTTP per accettare la richiesta
+    this.updateRequestStatus(requestId, status, userId);
+  }
+
+  rejectRequest(idRequest:number, isAccepted:boolean, idUser:number): void {
+    const requestId = idRequest;
+    const status = isAccepted;
+    const userId = idUser;
+
+    // Chiamata HTTP per accettare la richiesta
+    this.updateRequestStatus(requestId, status, userId);
+  }
+
+  private updateRequestStatus(
+    requestId: number,
+    status: boolean,
+    userId: number
+  ): void {
+    this.http.get<responseRequestAdmin>('http://localhost:5067/AdminRequest?id=' + requestId, { observe: "response"}).subscribe(
+      (response) => {
+        this.requestAdmins = response.body?.data;
+      },
+      (error) => {
+        console.error('Errore nel recupero dei dati:', error);
+      });
+  }
+
+
 }
 
 
 export interface RequestAdmin{
+  userId:number,
+  idRequest:number,
   date: Date,
   user: User
 }
@@ -44,3 +79,4 @@ export interface responseRequestAdmin {
   success: boolean,
   message: string
 }
+
